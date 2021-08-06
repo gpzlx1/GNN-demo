46cfaf666a4d58bec43ab7d639957fe6c72aeee8
pinggong <pinggong@amazon.com>
2021-08-06 05:42:53 +0000

    simplify graphsage

--- graphsage/train_sampling.py
+++ graphsage/train_sampling.py
@@ -8,31 +8,12 @@ import dgl.nn.pytorch as dglnn
 import time
 import argparse
 import tqdm
+from dgl import backend
 
 from model import SAGE
 from load_graph import load_reddit, inductive_split, load_ogb
+from sampler import sample_block
 
-def compute_acc(pred, labels):
-    """
-    Compute the accuracy of prediction given the labels.
-    """
-    labels = labels.long()
-    return (th.argmax(pred, dim=1) == labels).float().sum() / len(pred)
-
-def evaluate(model, g, nfeat, labels, val_nid, device):
-    """
-    Evaluate the model on the validation set specified by ``val_nid``.
-    g : The entire graph.
-    inputs : The features of all the nodes.
-    labels : The labels of all the nodes.
-    val_nid : the node Ids for validation.
-    device : The GPU device to evaluate on.
-    """
-    model.eval()
-    with th.no_grad():
-        pred = model.inference(g, nfeat, device, args.batch_size, args.num_workers)
-    model.train()
-    return compute_acc(pred[val_nid], labels[val_nid].to(pred.device))
 
 def load_subtensor(nfeat, labels, seeds, input_nodes, device):
     """
@@ -45,41 +26,23 @@ def load_subtensor(nfeat, labels, seeds, input_nodes, device):
 #### Entry point
 def run(args, device, data):
     # Unpack data
-    n_classes, train_g, val_g, test_g, train_nfeat, train_labels, \
-    val_nfeat, val_labels, test_nfeat, test_labels = data
+    n_classes, train_g, train_nfeat, train_labels = data
     in_feats = train_nfeat.shape[1]
     train_nid = th.nonzero(train_g.ndata['train_mask'], as_tuple=True)[0]
-    val_nid = th.nonzero(val_g.ndata['val_mask'], as_tuple=True)[0]
-    test_nid = th.nonzero(~(test_g.ndata['train_mask'] | test_g.ndata['val_mask']), as_tuple=True)[0]
 
-    val_nfeat = test_nfeat = train_nfeat
-
-    dataloader_device = th.device('cpu')
-    if args.sample_gpu:
-        train_nid = train_nid.to(device)
-        # copy only the csc to the GPU
-        train_g = train_g.formats(['csc'])
-        train_g = train_g.to(device)
-        dataloader_device = device
-
-    # Create PyTorch DataLoader for constructing blocks
-    sampler = dgl.dataloading.MultiLayerNeighborSampler(
-        [int(fanout) for fanout in args.fan_out.split(',')])
-    dataloader = dgl.dataloading.NodeDataLoader(
-        train_g,
-        train_nid,
-        sampler,
-        device=dataloader_device,
-        batch_size=args.batch_size,
-        shuffle=True,
-        drop_last=False,
-        num_workers=args.num_workers)
-
+    idx = th.randperm(train_nid.nelement())
+    train_nid = train_nid[idx]
+    train_nid = train_nid.to(device)
+    train_g = train_g.formats(['csc'])
+    train_g = train_g.to("cuda")
+    
     # Define model and optimizer
     model = SAGE(in_feats, args.num_hidden, n_classes, args.num_layers, F.relu, args.dropout)
     model = model.to(device)
     loss_fcn = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
+
+    fan_out = [int(fanout) for fanout in args.fan_out.split(',')]
 
     # Training loop
     avg = 0
@@ -87,11 +50,18 @@ def run(args, device, data):
     for epoch in range(args.num_epochs):
         tic = time.time()
 
-        # Loop over the dataloader to sample the computation dependency graph as a list of
-        # blocks.
         tic_step = time.time()
-        for step, (input_nodes, seeds, blocks) in enumerate(dataloader):
+        for step in range((train_nid.nelement() + args.batch_size - 1) // args.batch_size):
             # Load the input features as well as output labels
+
+            seeds = train_nid[step * args.batch_size : (step + 1) * args.batch_size]
+            blocks = []
+            nodes_all_types = [backend.to_dgl_nd(seeds)]
+            for num_picks in fan_out:
+                block, nodes_all_types = sample_block(train_g, nodes_all_types, 10, False)
+                blocks.insert(0, block)
+            input_nodes = blocks[0].srcdata[dgl.NID]
+            
             batch_inputs, batch_labels = load_subtensor(train_nfeat, train_labels,
                                                         seeds, input_nodes, device)
             blocks = [block.to(device) for block in blocks]
@@ -105,21 +75,16 @@ def run(args, device, data):
 
             iter_tput.append(len(seeds) / (time.time() - tic_step))
             if step % args.log_every == 0:
-                acc = compute_acc(batch_pred, batch_labels)
+                acc = 0
                 gpu_mem_alloc = th.cuda.max_memory_allocated() / 1000000 if th.cuda.is_available() else 0
                 print('Epoch {:05d} | Step {:05d} | Loss {:.4f} | Train Acc {:.4f} | Speed (samples/sec) {:.4f} | GPU {:.1f} MB'.format(
-                    epoch, step, loss.item(), acc.item(), np.mean(iter_tput[3:]), gpu_mem_alloc))
+                    epoch, step, loss.item(), acc, np.mean(iter_tput[3:]), gpu_mem_alloc))
             tic_step = time.time()
 
         toc = time.time()
-        print('Epoch Time(s): {:.4f}'.format(toc - tic))
         if epoch >= 5:
             avg += toc - tic
-        if epoch % args.eval_every == 0 and epoch != 0:
-            eval_acc = evaluate(model, val_g, val_nfeat, val_labels, val_nid, device)
-            print('Eval Acc {:.4f}'.format(eval_acc))
-            test_acc = evaluate(model, test_g, test_nfeat, test_labels, test_nid, device)
-            print('Test Acc: {:.4f}'.format(test_acc))
+        print('Epoch Time(s): {:.4f}'.format(toc - tic))
 
     print('Avg epoch time: {}'.format(avg / (epoch - 4)))
 
@@ -134,27 +99,11 @@ if __name__ == '__main__':
     argparser.add_argument('--fan-out', type=str, default='10,25')
     argparser.add_argument('--batch-size', type=int, default=1000)
     argparser.add_argument('--log-every', type=int, default=20)
-    argparser.add_argument('--eval-every', type=int, default=5)
     argparser.add_argument('--lr', type=float, default=0.003)
     argparser.add_argument('--dropout', type=float, default=0.5)
-    argparser.add_argument('--num-workers', type=int, default=4,
-                           help="Number of sampling processes. Use 0 for no extra process.")
-    argparser.add_argument('--sample-gpu', action='store_true',
-                           help="Perform the sampling process on the GPU. Must have 0 workers.")
-    argparser.add_argument('--inductive', action='store_true',
-                           help="Inductive learning setting")
-    argparser.add_argument('--data-cpu', action='store_true',
-                           help="By default the script puts all node features and labels "
-                                "on GPU when using it to save time for data copy. This may "
-                                "be undesired if they cannot fit in GPU memory at once. "
-                                "This flag disables that.")
     args = argparser.parse_args()
 
-    if args.gpu >= 0:
-        device = th.device('cuda:%d' % args.gpu)
-    else:
-        device = th.device('cpu')
-
+    device = th.device('cuda:%d' % args.gpu)
     if args.dataset == 'reddit':
         g, n_classes = load_reddit()
     elif args.dataset == 'ogbn-products':
@@ -162,25 +111,13 @@ if __name__ == '__main__':
     else:
         raise Exception('unknown dataset')
 
-    if args.inductive:
-        train_g, val_g, test_g = inductive_split(g)
-        train_nfeat = train_g.ndata.pop('features')
-        val_nfeat = val_g.ndata.pop('features')
-        test_nfeat = test_g.ndata.pop('features')
-        train_labels = train_g.ndata.pop('labels')
-        val_labels = val_g.ndata.pop('labels')
-        test_labels = test_g.ndata.pop('labels')
-    else:
-        train_g = val_g = test_g = g
-        train_nfeat = val_nfeat = test_nfeat = g.ndata.pop('features')
-        train_labels = val_labels = test_labels = g.ndata.pop('labels')
+    train_g =  g
+    train_nfeat = g.ndata.pop('features')
+    train_labels = g.ndata.pop('labels')
 
-    if not args.data_cpu:
-        train_nfeat = train_nfeat.to(device)
-        train_labels = train_labels.to(device)
+    train_nfeat = train_nfeat.to(device)
+    train_labels = train_labels.to(device)
 
     # Pack data
-    data = n_classes, train_g, val_g, test_g, train_nfeat, train_labels, \
-           val_nfeat, val_labels, test_nfeat, test_labels
-
+    data = n_classes, train_g, train_nfeat, train_labels
     run(args, device, data)
---
+++ sampler.py
@@ -0,0 +1,37 @@
+import torch as th
+
+import dgl.ndarray as nd
+from dgl import utils
+from dgl import backend
+from dgl.transform import _CAPI_DGLToBlock
+from dgl.heterograph import DGLHeteroGraph, DGLBlock
+from dgl.sampling.neighbor import _CAPI_DGLSampleNeighbors
+
+
+def sample_block(graph, nodes_all_types, fanout, replace=False):
+    th.cuda.nvtx.range_push("Sample Neighbors")
+    subgidx = _CAPI_DGLSampleNeighbors(
+            graph._graph, 
+            nodes_all_types,
+            backend.to_dgl_nd(backend.tensor([fanout], dtype=backend.int64)),
+            'in',  
+            [nd.array([], ctx=nd.cpu())] * len(graph.etypes),
+            replace
+        )
+    subgraph = DGLHeteroGraph(subgidx.graph, graph.ntypes, graph.etypes)
+    th.cuda.nvtx.range_pop()
+
+    th.cuda.nvtx.range_push("To Block")
+    new_graph_index, src_nodes_nd, induced_edges_nd = _CAPI_DGLToBlock(
+        subgraph._graph, nodes_all_types, True
+    )
+    block = DGLBlock(new_graph_index, (subgraph.ntypes, subgraph.ntypes), subgraph.etypes)
+
+    src_node_ids = [backend.from_dgl_nd(src) for src in src_nodes_nd]
+    dst_node_ids = [backend.from_dgl_nd(dst) for dst in nodes_all_types]
+
+    node_frames = utils.extract_node_subframes_for_block(subgraph, src_node_ids, dst_node_ids)
+    block._node_frames = node_frames
+    th.cuda.nvtx.range_pop()
+    
+    return block, src_nodes_nd
